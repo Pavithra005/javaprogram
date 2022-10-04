682967b4a4a859b81aa8ca77f7d830106afc1087
Pavithra005 <106576855+Pavithra005@users.noreply.github.com>
2022-10-04 18:55:04 +0530

    Update primenumber.java

--- primenumber.java
+++ primenumber.java
@@ -1,28 +1,19 @@
-class primenumber
-{
-   public static void main (String[] args)
-   {  
-       int i =0;
-       int num =0;
-       String primeNumbers = "";
+public class primenumber{  
+ public static void main(String args[]){
+        for(int i=2;i <= 50;i++)
+        {
+            boolean prime = true;
+            for(int j = 2;j < i;j++){
 
-       for (i = 1; i <= 100; i++)         
-       {        
-          int counter=0;    
-          for(num =i; num>=1; num--)
-   {
-             if(i%num==0)
-      {
-   counter = counter + 1;
-      }
-   }
-   if (counter ==2)
-   {
-      
-      primeNumbers = primeNumbers + i + " ";
-   } 
-       } 
-       System.out.println("Prime numbers from 1 to 100 are :");
-       System.out.println(primeNumbers);
-   }
+                if(i%j == 0)
+                {
+                    prime = false;
+                    break;
+                }
+            }
+            if(prime)
+            { System.out.println(i);
+            }
+        }
+    }
 }
